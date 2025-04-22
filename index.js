3b0113f124a36c0f3c3d72f832edf7947e38bbd0
Tarun Kumar <tarunkuma07@gmail.com>
2025-04-22 16:16:08 +0530

    Search API in mongodb using nodejs

--- index.js
+++ index.js
@@ -162,31 +162,50 @@ require('./config');
 const Product = require('./product');
 const app = express();
 app.use(express.json());
-app.post('/create',async(req,resp)=>{
-    let data = new Product(req.body);
-    let result = await data.save();
-    console.log(result);
-    resp.send(result);
-});
-
-app.get('/list',async(req,resp)=>{
-    let data = Product.find();
-    resp.send(data);
-});
-
-app.delete('/delete/:_id',async(req,resp)=>{
-    console.log(req.params);
-    let data = await Product.deleteOne(req.params)
-    resp.send(data);
-});
-
-app.put('/update/:_id', async(req,resp)=>{
-    console.log(req.params);
-    let data = await Product.updateOne(
-        req.params,{
-            $set:req.body
+app.get("/search/:key",async (req,resp)=>{
+    console.log(req.params.key);
+    let data = await Product.find(
+        {
+            "$or":[
+                {
+                    "name":{$regex:req.params.key}
+                },
+                {
+                    "brand":{$regex:req.params.key}
+                },
+                {
+                    "category":{$regex:req.params.key}
+                }
+            ]
         }
     )
     resp.send(data);
-});
+})
+// app.post('/create',async(req,resp)=>{
+//     let data = new Product(req.body);
+//     let result = await data.save();
+//     console.log(result);
+//     resp.send(result);
+// });
+
+// app.get('/list',async(req,resp)=>{
+//     let data = Product.find();
+//     resp.send(data);
+// });
+
+// app.delete('/delete/:_id',async(req,resp)=>{
+//     console.log(req.params);
+//     let data = await Product.deleteOne(req.params)
+//     resp.send(data);
+// });
+
+// app.put('/update/:_id', async(req,resp)=>{
+//     console.log(req.params);
+//     let data = await Product.updateOne(
+//         req.params,{
+//             $set:req.body
+//         }
+//     )
+//     resp.send(data);
+// });
 app.listen(5000);
